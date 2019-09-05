d7774d70fb14af5b69b7f1e37385ee089e09f3c0
lab1-xy20192019 <xy20192019@gmail.com>
2019-09-04 23:57:41 -0700

    Cody first lab

--- node/functions.js
+++ node/functions.js
@@ -12,10 +12,27 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
-
 // make an arrow function with no block body
+const arrowNoBody = (arg) => 67 + 2324;
+const singleArg = arg => arg + 3; //single arg doesn't need curly braces
+console.log(singleArg(6));
 
 // make a function that returns a function
+const someFunction =(arg) => console.log(arg);
+const functionFactory = arg => () => someFunction(arg);
+functionFactory('Hello World!')();
+
+(() => {
+  //calls itself
+})();
+
+//recursively calling itself in every second
+const aFunction = () => {
+  console.log("Goodbye");
+  setTimeout(aFunction, 1000);
+};
+
+aFunction();
